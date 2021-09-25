18934c3735578a0d203dbec205ea67b5c7ce201a
petrashin <88317323+petrashin@users.noreply.github.com>
2021-09-25 22:02:06 +0300

    Step 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+from telebot import types
 from dotenv import load_dotenv
 
 
@@ -9,17 +10,70 @@ TOKEN = os.getenv('TOKEN')
 
 bot = telebot.TeleBot(TOKEN)
 
+name = ''
+surname = ''
+age = 0
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, 'Запомню : )')
+
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+def start(message):
     if message.text.lower() == "привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == '/hello-world':
-        bot.send_message(message.from_user.id, "Привет, ты ввел команду /hello-world")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "Я тебя не понимаю, введи команду /help")
+        bot.register_next_step_handler(message, help_func)
+
+
+def help_func(message):
+    if message.text == '/help':
+        bot.send_message(message.from_user.id, "Это функция-помощник, но она сейчас недоступна")
+
+
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, "Какая у тебя фамилия?")
+    bot.register_next_step_handler(message, get_surname)
+
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, "Сколько тебе лет?")
+    bot.register_next_step_handler(message, get_age)
+
+
+def get_age(message):
+    global age
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, "Цифрами, пожалуйста")
+
+    keyboard = types.InlineKeyboardMarkup()
+
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+
+    question = "Тебе {} лет и тебя зовут {} {}?".format(
+        age,
+        name,
+        surname
+    )
+
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
 
 bot.polling(none_stop=True, interval=0)
